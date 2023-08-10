33df06d6e4ed220e70c19fa04d41db4dee60a49e
Ali Bayram <benalibayram@gmail.com>
2023-08-11 00:27:39 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,13 @@ release = '0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-	"sphinx.ext.duration",
-	"sphinx_rtd_theme",
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+	'sphinx.ext.duration',
+	'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
@@ -30,3 +35,20 @@ language = 'tr'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_theme_path = ["_themes", ]
+
+html_theme_options = {
+    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
